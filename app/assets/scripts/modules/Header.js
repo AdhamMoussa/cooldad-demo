6f048587a1fa7bc39f51428c8d36c42710df5bca
Adham Moussa <adam.moussa62017@gmail.com>
2018-07-13 20:57:47 +0200

    finished header functionality

--- app/assets/scripts/modules/Header.js
+++ app/assets/scripts/modules/Header.js
@@ -1,21 +1,39 @@
 import $ from 'jquery';
-import SmoothScroll from '../../../node_modules/smooth-scroll/dist/smooth-scroll.polyfills';
+import SmoothScroll from '../../../../node_modules/smooth-scroll/dist/smooth-scroll.polyfills';
 
 
 export default class Header {
   constructor() {
     this.window = $(window);
     this.header = $('#header');
+    this.sections = $('.section');
+    this.navElements = $('.header__nav-list-item');
     this.scroll = new SmoothScroll('a[href*="#"]', {speed: 1000});
-    this.toggleSolidHeader();
+    this.triggerHeader();
   }
-  toggleSolidHeader() {
-    this.window.scroll(() => {
-      if (this.window.scrollTop() > 150) {
-        this.header.addClass('header--solid');
+  triggerHeader() {
+    const that = this;
+    this.window.scroll(function () {
+      // solid header on scroll
+      if (that.window.scrollTop() > 150) {
+        that.header.addClass('header--solid');
       } else {
-        this.header.removeClass('header--solid');
+        that.header.removeClass('header--solid');
       }
+
+      // link sync with sections
+      if (that.window.scrollTop() < 300) {
+        that.navElements.removeClass('header__nav-list-item--active');
+      }
+
+      that.sections.each(function () {
+        if (that.window.scrollTop() > $(this).offset().top - 300) {
+          const target = '#'+$(this).attr('id');
+          console.log($('[href="'+target+'"]'));
+          
+          $('[href="'+target+'"]').parent().addClass('header__nav-list-item--active').siblings().removeClass('header__nav-list-item--active');
+        }
+      });
     });
-  }
+  };
 }
