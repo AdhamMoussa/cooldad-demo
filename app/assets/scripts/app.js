40e997ab4de08a3918d41a84c8ba6764c406387f
Adham Moussa <adam.moussa62017@gmail.com>
2018-07-16 15:43:08 +0200

    responsive images

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,4 +1,13 @@
 import Header from './modules/Header';
+import $ from 'jquery';
+
+// load section images only if window width > 992px
+if ($(window).width() > 992) {
+  $('.desktop-img').each(function () {
+    $(this).attr('src', $(this).attr('data-src'));
+  });
+}
+
 
 const header = new Header();
 
